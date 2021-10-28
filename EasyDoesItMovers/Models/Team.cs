572b9e827dd1db991aa953bc80aa18c8554291d4
N3ich <60138338+N3ich@users.noreply.github.com>
2021-10-27 23:02:09 -0400

    StartingImplementingDbContext

---
+++ EasyDoesItMovers/Models/Team.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyDoesItMovers.Models
+{
+    public class Team
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Position { get; set; }
+        [Required]
+        public string ShortDescription { get; set; }
+        [Required]
+        public string Text { get; set; }
+        public byte[] ImageData { get; set; }
+    }
+}
