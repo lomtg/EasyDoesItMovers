4c1833e65fd7468d5ae88cc3c7f0ffb219656da5
N3ich <60138338+N3ich@users.noreply.github.com>
2021-10-30 01:55:37 -0400

    AddedLoginViewAndAdminLayout

---
+++ EasyDoesItMovers/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyDoesItMovers.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ EasyDoesItMovers/Models/Login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyDoesItMovers.Models
+{
+    public class Login
+    {
+        public String Username { get; set; }
+        public String Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
